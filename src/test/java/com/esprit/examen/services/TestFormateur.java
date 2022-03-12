8764e4d562b91faa518e81b43350f00fbb6dad28
zeinebfekihahmed <ahmedzeineb.benfekih@esprit.tn>
2022-03-12 12:40:21 +0100

    formation repo and services classes

--- src/test/java/com/esprit/examen/services/TestFormateur.java
+++ src/test/java/com/esprit/examen/services/TestFormateur.java
@@ -25,14 +25,14 @@ public class TestFormateur {
 	@Autowired
 	FormateurRepository formateurRepository;
 	private static Contrat CONTRAT = Contrat.CIVP;
-	private static String EMAIL = "felBenFoulen@gmail.com";
+	private static String EMAIL = "ahmedzeineb.benfekih@esprit.tn";
 	private static String PASSWORD ="password" ;
-	private static String NOM = "flen";
-	private static String PRENOM = "ben foulen";
+	private static String NOM = "zeineb";
+	private static String PRENOM = "benfekih";
 	private static Poste POST=  Poste.Ingénieur;
 	private Formateur testformateur = new Formateur( NOM, PRENOM, POST, CONTRAT, EMAIL, PASSWORD);
     private Formateur testformateur2 = new Formateur( "f1", "P1", Poste.Technicien, Contrat.EXPERT, "f1@gamil.com", "passF1");
-    private Formateur testformateur3 = new Formateur( "f2", "P1", Poste.Ingénieur, Contrat.FREELANCE, "f2@gmail.com", "PasF2");
+    private Formateur testformateur3 = new Formateur( "f2", "P1", Poste.Ingénieur, Contrat.FREELANCE, "f2@gmail.com", "PassF2");
 private static final Logger logger = LogManager.getLogger(TestFormateur.class);
  
 	@BeforeEach
@@ -43,21 +43,21 @@ private static final Logger logger = LogManager.getLogger(TestFormateur.class);
 	public void testAddFormateur(){
 	logger.debug("entring testAddFormateur methode");
 	long start = System.currentTimeMillis();
-	formateurService.addFormateur(testformateur);
-		assertThat(formateurService.listFormateurs().contains(testformateur));
+	formateurService.addFormateur(testformateur2);
+		assertThat(formateurService.listFormateurs().contains(testformateur2));
 		long elapsedTime = System.currentTimeMillis() - start ;
 	logger.info("Method execution time: " + elapsedTime + " milliseconds.");
 	logger.debug("quiting testAddFormateur methode");
-	formateurService.supprimerFormateur(testformateur.getId());
+	formateurService.supprimerFormateur(testformateur2.getId());
 	}
 	@Test
 	public void testDeleteFormateur(){
 	logger.debug("entring testDeleteFormateur methode");
 	long start = System.currentTimeMillis();
-	formateurService.addFormateur(testformateur);
-		assertThat(formateurService.listFormateurs().contains(testformateur));
-     formateurService.supprimerFormateur(testformateur.getId());
-     assertThat(formateurService.listFormateurs().contains(testformateur)==false);
+	formateurService.addFormateur(testformateur2);
+		assertThat(formateurService.listFormateurs().contains(testformateur2));
+     formateurService.supprimerFormateur(testformateur2.getId());
+     assertThat(formateurService.listFormateurs().contains(testformateur2)==false);
 		long elapsedTime = System.currentTimeMillis() - start ;
 	logger.info("Method execution time: " + elapsedTime + " milliseconds.");
 	logger.debug("quiting testAddFormateur methode");	
@@ -66,13 +66,13 @@ private static final Logger logger = LogManager.getLogger(TestFormateur.class);
 	public void testmodifierFormateur(){
 		logger.debug("entring testmodifierFormateur methode");
 		long start = System.currentTimeMillis();
-        formateurService.addFormateur(testformateur);
-		testformateur.setNom("new flen");
-		System.out.print(testformateur.getNom());
+        formateurService.addFormateur(testformateur2);
+		testformateur.setNom("ahmed");
+		System.out.print(testformateur2.getNom());
 		testformateur.setPassword("new password");
-		formateurService.modifierFormateur(testformateur);
+		formateurService.modifierFormateur(testformateur2);
 		Formateur formateurModifier = (Formateur) formateurService.listFormateurs().get(0);
-		assertThat("new flen").isEqualTo(formateurModifier.getNom());
+		assertThat("f1").isEqualTo(formateurModifier.getNom());
 		assertThat("new password").isEqualTo(formateurModifier.getPassword());
 		long elapsedTime = System.currentTimeMillis() - start ;
 		logger.info("Method execution time: " + elapsedTime + " milliseconds.");
